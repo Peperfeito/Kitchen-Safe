b349ed0a391c67c92eef8e7c8cff4a5c9c62b7af
Dani <39123021+bartolomeux1@users.noreply.github.com>
2024-05-19 22:28:13 -0300

    porco

---
+++ My project/Assets/Scripts/Pig.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    public float maxSpeed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, maxSpeed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+    private void OnMouseEnter()
+    {
+        maxSpeed = 0;
+    }
+    private void OnMouseExit()
+    {
+        maxSpeed = 10;
+    }
+    }
