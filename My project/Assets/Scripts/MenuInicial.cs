581e684b3647bd5c0ade1abaae7dbca6632e3ba1
Peperfeito <gabrielcarvalhoesilva@gmail.com>
2024-05-22 16:15:12 -0300

    versão ainda sem os porcos e as artes
    Mas fiz um menuzinho do jogo logo vou substituir as artes ainda hj

--- My project/Assets/Scripts/MenuInicial.cs
+++ My project/Assets/Scripts/MenuInicial.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuInicial : MonoBehaviour
 {
+    [SerializeField] GameObject opcao;
+    [SerializeField] GameObject legendas;
     public void LoadScene(string cena)
     {
         SceneManager.LoadScene(cena);
@@ -12,6 +14,26 @@ public class MenuInicial : MonoBehaviour
         
 
 
+    }
+
+    public void Options()
+    {
+        opcao.SetActive(true);
+    }
+
+    public void BackOptions()
+    {
+        opcao.SetActive(false);
+    }
+
+    public void Legends()
+    {
+        legendas.SetActive(true);
+    }
+
+    public void BackLegends()
+    {
+        legendas.SetActive(false);
     }
 
     public void Quit()
